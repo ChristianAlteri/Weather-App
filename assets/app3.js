72a7fca98cf2128b5170241c978b2a590b99169a
ChristianAlteri <alteri.christian@gmail.com>
2023-04-25 18:37:01 +0800

    api bugs

--- assets/app3.js
+++ assets/app3.js
@@ -10,46 +10,53 @@ for (let i = 0; i < 5; i++) {
 
 console.log(days);
 
-let exampleUrl = "http://api.openweathermap.org/data/2.5/weather?q=denver&appid=37d5a1d043d687cb9e9c0189aad1636a"
+
 const APIKey = '37d5a1d043d687cb9e9c0189aad1636a'
 // let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// "https://api.openweathermap.org/data/2.5/onecall?appid=37d5a1d043d687cb9e9c0189aad1636a&q=" + city + "&units=imperial&exclude=minutely,hourly"
 
-
+    let inputElement = document.getElementById('input-search');
+    let city =  inputElement.value;
+    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    
 
 function citySearch(event) {
     event.preventDefault()
-    let inputElement = document.getElementById('input-search');
-    let city =  inputElement.value;
+    cityCord();
+    weatherData();
     
-    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    // Retrieves the data for the city
-        fetch(queryURL)
-        .then(function (response) {
-            return response.json();
-            
-        }).then(function (result){
-            console.log(result);
-            let lon = result.coord.lon
-            let lat = result.coord.lat
-            let newURL = "https://api.openweathermap.org/data/2.5/onecall?appid=37d5a1d043d687cb9e9c0189aad1636a&q=" + city + "&units=imperial&exclude=minutely,hourly"
-            let cityName = result.name
-            let temp = result.main.temp
-            let wind = result.wind.speed
-            let humidity = result.main.humidity
-            let icon = result.weather[0].icon
-            recentSearches.push({Name: cityName});
-            localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
-            $("#weather-card-container").empty();
-            $("#display-card").empty();
-            displayCurrentDayWeather(temp, wind, humidity);
-            displayForecast(cityName, temp, wind, humidity);
-            createCityList(cityName);
-            console.log(result);
-            // recentSearches.push({ Name: cityName, longitude: longitude , latitude: latitude, temp: temp, wind: wind, humidity: humidity});
-            
 
-               
-        })
+    // // Retrieves the data for the city
+    //     fetch(queryURL)
+    //     .then(function (response) {
+    //         return response.json();
+            
+    //     }).then(function (result){
+    //         console.log(result);
+    //         
+    //     }   fetch(newURL)
+    //         .then(function) (result){
+    //         // recentSearches.push({ Name: cityName, longitude: longitude , latitude: latitude, temp: temp, wind: wind, humidity: humidity});
+            
+    //         let cityName = result.name
+    //         let temp = result.main.temp
+    //         let wind = result.wind.speed
+    //         let humidity = result.main.humidity
+    //         const iconID = weatherData.current.weather[0]['icon'];
+    //         const iconURL = "http://openweathermap.org/img/w/" + iconID + ".png";
+    //         console.log(iconURL)
+    //         recentSearches.push({Name: cityName});
+    //         localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
+    //         $("#weather-card-container").empty();
+    //         $("#display-card").empty();
+    //         displayCurrentDayWeather(temp, wind, humidity);
+    //         displayForecast(cityName, temp, wind, humidity);
+    //         createCityList(cityName);
+    //         console.log(result);
+
+    //         }    
+    //     })
     
     } 
 
@@ -123,5 +130,29 @@ function citySearch(event) {
         };
 
 
-
-
+       
+        
+        // Find the coordinates of the city I've searched
+        function cityCord() {
+            return fetch(queryURL)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (result) {
+                let lon = result.coord.lon;
+                let lat = result.coord.lat;
+                console.log(result);
+                let newURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=37d5a1d043d687cb9e9c0189aad1636a`;
+                return weatherData(newURL); 
+              });
+          }
+          
+          function weatherData(newURL) {
+            return fetch(newURL)
+              .then(function (response) {
+                return response.json();
+                console.log(cityCord);
+                console.log(weatherData);
+              });
+          }
+          
